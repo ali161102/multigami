f4a86e8d0eefa58e138a63a3bba3556765f95d2a
ali161102 <ali16miller@hotmail.co.uk>
2025-05-24 02:41:46 +0100

    24/05/25 export

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ console.dir(table)
 let row = table.querySelector("#labels")
 console.dir(row)
 
-const lim_a = 66;
-const lim_s = 65;
+const lim_a = 70;
+const lim_s = 70;
 
 
 const tableBody = table.querySelector("tbody")
@@ -100,6 +100,33 @@ for (let outcome in counts) {
             tile.style.backgroundColor = "#008000";
         }
     }
+
+
+}
+
+let resultText = document.getElementById("resultText")
+resultText.onclick("handleResultText()")
+
+function handleResultText() {
+    console.log("yes")
+    for (let outcome in counts) {
+        let attempted = outcome%100;
+        let solved = (outcome - attempted)/100;
+        console.log(outcome, attempted, solved)
+        let result = solved + '/' + attempted;
+    
+        let tile = document.getElementById(result);
+        if (tile) {
+            tile.classList.add("acheived");
+    
+            //tile.innerText = result;
+            /////////// number of occurrences text
+            tile.innerText = counts[outcome]
+    
+        }
+    
+    
+    }f
 
 
 }
